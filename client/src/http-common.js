0a9cb802d5f0fecb7b2cdad0a602cc935d262128
TrnCngH <hautrancong@gmail.com>
2023-03-01 07:18:28 +0700

    cicd: first build

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3084/api",
+  // baseURL: "http://localhost:3084/api",
+  baseURL: "http://192.168.1.101:3084/api",
   headers: {
     "Content-type": "application/json",
   },
