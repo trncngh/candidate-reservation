f8bd629d5ac3fcc59d4b96f9f54a91d4fce74dc9
TrnCngH <hautrancong@gmail.com>
2023-03-02 08:16:47 +0700

    feat: edit axios header

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3084/api",
-  baseURL: "http://172.31.41.65:3084/api",
+  baseURL: "https://172.31.41.65:3084/api",
   headers: {
     "Content-type": "application/json",
-    // "Access-Control-Allow-Origin": "*",
-    // "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    // "Access-Control-Allow-Headers":
-    //   "x-access-token, Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    "Access-Control-Allow-Headers":
+      "x-access-token, Origin, X-Requested-With, Content-Type, Accept",
   },
 });
